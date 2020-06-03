0fa1d87bd2f627c4c2c86dc37aa81125c0d3e68d
Francis <francisseneve@gmail.com>
2020-06-03 02:49:59 +0200

    End of authentification with jwt

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,13 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/user');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const { JWT_SECRET } = require('../keys');
+const requireLogin = require('../middleware/requireLogin');
+
+router.get('/protected', requireLogin, (req, res) => {
+    res.send('Hello user');
+})
 
 router.post('/signup', (req, res) => {
     const { name, email, password } = req.body;
@@ -35,7 +42,30 @@ router.post('/signup', (req, res) => {
 
 router.post('/signin', (req, res) => {
     const { email, password } = req.body;
+    if (!email || !password) {
+        return res.status(422).json({ "error": "Please add email or password" });
+    }
 
+    User.findOne({ email: email })
+        .then(savedUser => {
+            if (!savedUser) {
+                return res.status(422).json({ "error": "Invalid email or password" });
+            }
+
+            bcrypt.compare(password, savedUser.password)
+                .then(doMatch => {
+                    if (doMatch) {
+                        //res.json({ "message": "Successfully signed in" });
+                        const token = jwt.sign({ _id: savedUser._id }, JWT_SECRET);
+                        res.json({ token: token });
+                    } else {
+                        return res.status(422).json({ "error": "Invalid password" });
+                    }
+                })
+        })
+        .catch(error => {
+            console.log(error);
+        })
 });
 
 module.exports = router;
